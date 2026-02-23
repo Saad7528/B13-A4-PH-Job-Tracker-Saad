29bc9137f1919d907c86236f25ec78d8ef92d8cf
S.M. Amirul Islam Saad <saad0174742@gmail.com>
2026-02-23 09:41:07 +0600

    add all count

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-const rbtn = document.getElementById('R-btn')
-console.log(rbtn.innerText)
- 
+interviewList = [];
+rejectedList = [];
+
+// call counter id
+const totalCount = document.getElementById('total-result')
+const interviewCount = document.getElementById('interview-result')
+const rejectedCount = document.getElementById('rejected-result')
+
+// call card container id
+const cardContainer = document.getElementById('cards-container')
+
+// call side note id
+const sideNote = document.getElementById('side-note')
+
+// set value for total count and side note
+totalCount.innerText = cardContainer.children.length
+sideNote.innerText = cardContainer.children.length
+
+
+
+// Call filter buttons
+const allBtn = getElementById('btn-all-cards')
+const interviewBtn = getElementById('btn-interview-cards')
+const rejectedBtn = getElementById('btn-rejected-cards')
+
+
+const btnContainer = document.getAnimations('btn-container')
+console.log(btnContainer)
