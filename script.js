031802b1923abb0e8da89c0313da9d1a265e5817
S.M. Amirul Islam Saad <saad0174742@gmail.com>
2026-02-23 12:40:39 +0600

    set toggle btn style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-interviewList = [];
-rejectedList = [];
-
+let interviewList = [{name: 'saad'},{name: 'saad'}];
+let rejectedList = [{name: 'saad'},{name: 'saad'},{name: 'saad'}];
+ 
 // call counter id
 const totalCount = document.getElementById('total-result')
 const interviewCount = document.getElementById('interview-result')
@@ -15,14 +15,40 @@ const sideNote = document.getElementById('side-note')
 // set value for total count and side note
 totalCount.innerText = cardContainer.children.length
 sideNote.innerText = cardContainer.children.length
+// value set interview and rejected
+interviewCount.innerText = interviewList.length
+rejectedCount.innerText = rejectedList.length
 
+// function calculateCount(){
+//     totalCount.innerText = cardContainer.children.length
+//     sideNote.innerText = cardContainer.children.length
 
+//     interviewCount.innerText = interviewList.length
+//     rejectedCount.innerText = rejectedList.length
+// }
+// calculateCount()
 
 // Call filter buttons
-const allBtn = getElementById('btn-all-cards')
-const interviewBtn = getElementById('btn-interview-cards')
-const rejectedBtn = getElementById('btn-rejected-cards')
+const allBtn = document.getElementById('btn-all-cards')
+const interviewBtn = document.getElementById('btn-interview-cards')
+const rejectedBtn = document.getElementById('btn-rejected-cards')
 
 
-const btnContainer = document.getAnimations('btn-container')
-console.log(btnContainer)
+function toggleBtnStyle(id){
+
+    // remove style from btns
+    allBtn.classList.remove('btn-info','text-[#FFFFFF]')
+    interviewBtn.classList.remove('btn-info','text-[#FFFFFF]')
+    rejectedBtn.classList.remove('btn-info','text-[#FFFFFF]')
+
+    // catch element id and set a variable
+    let select = document.getElementById(id);
+
+    // add selected btn style
+    select.classList.add('btn-info','text-[#FFFFFF]')
+
+
+}
+
+// const btnContainer = document.getAnimations('btn-container')
+// console.log(btnContainer)
